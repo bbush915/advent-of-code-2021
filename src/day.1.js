43bb227db695658c54cf2bced4abd0ab034a8a09
Bryan Bush <bryan@dialexa.com>
2021-11-30 23:09:24 -0600

    Day 1

--- src/day.1.js
+++ src/day.1.js
@@ -1,12 +1,40 @@
 const fs = require("fs");
 
 function parseInput() {
-  return fs.readFileSync("./day.1.input.txt", "utf-8");
+  return fs
+    .readFileSync("src/day.1.input.txt", "utf-8")
+    .split("\n")
+    .filter((x) => x)
+    .map((x) => Number(x));
 }
 
-function part1() {}
+function part1() {
+  const measurements = parseInput();
 
-function part2() {}
+  let count = 0;
+
+  for (let i = 1; i < measurements.length; i++) {
+    if (measurements[i] > measurements[i - 1]) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function part2() {
+  const measurements = parseInput();
+
+  let count = 0;
+
+  for (let i = 3; i < measurements.length; i++) {
+    if (measurements[i] > measurements[i - 3]) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports.part1 = part1;
 module.exports.part2 = part2;
