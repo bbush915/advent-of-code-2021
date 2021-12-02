b11c81c03e57078dff7bbd75f32d5fdc62d90dfd
Bryan Bush <bryan@dialexa.com>
2021-12-01 23:10:11 -0600

    Day 2 JS

--- src/day.2.js
+++ src/day.2.js
@@ -1,12 +1,71 @@
 const fs = require("fs");
 
 function parseInput() {
-  return fs.readFileSync("src/day.2.input.txt", "utf-8");
+  return fs
+    .readFileSync("src/day.2.input.txt", "utf-8")
+    .split("\n")
+    .filter((x) => x)
+    .map((x) => {
+      const parts = x.split(" ");
+
+      return {
+        direction: parts[0],
+        value: Number(parts[1]),
+      };
+    });
 }
 
-function part1() {}
+function part1() {
+  const commands = parseInput();
 
-function part2() {}
+  let position = 0;
+  let depth = 0;
+
+  for (let i = 0; i < commands.length; i++) {
+    const command = commands[i];
+
+    if (command.direction === "forward") {
+      position += command.value;
+    }
+
+    if (command.direction === "down") {
+      depth += command.value;
+    }
+
+    if (command.direction === "up") {
+      depth -= command.value;
+    }
+  }
+
+  return position * depth;
+}
+
+function part2() {
+  const commands = parseInput();
+
+  let position = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for (let i = 0; i < commands.length; i++) {
+    const command = commands[i];
+
+    if (command.direction === "forward") {
+      position += command.value;
+      depth += aim * command.value;
+    }
+
+    if (command.direction === "down") {
+      aim += command.value;
+    }
+
+    if (command.direction === "up") {
+      aim -= command.value;
+    }
+  }
+
+  return position * depth;
+}
 
 module.exports.part1 = part1;
 module.exports.part2 = part2;
